44ba5d4cc8eab8d5a8a8af445a1d349525066e8b
manoj <manojbramhankar9393@gmail.com>
2023-03-20 20:11:27 +0530

    first commit

---
+++ demo/src/com/demo/Use.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+import java.util.ArrayList;
+
+public class Use {
+
+	public static void main(String[] args) {
+		 ArrayList use=new ArrayList<>();
+		 use.add(101);
+		 use.add(102);
+		 use.add(103);
+		 use.add(104);
+		 use.add(105);
+		 use.add(102);
+		 System.out.println(use);
+	}
+
+}
